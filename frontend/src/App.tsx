08366d9c518a48e2807ad40b9ee521b2d73ad58f
José Naour <jo.naour@duocuc.cl>
2026-05-14 18:33:30 -0400

    Revert "feat: add Login Page & React Router"
    This reverts commit 7932db968345a72ad13d77059d080a22cba50c8b.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
-import {Routes, Route} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage></LoginPage>} />
-    </Routes>
+    <>
+      <LandingPage></LandingPage>
+    </>
   );
 }
 
